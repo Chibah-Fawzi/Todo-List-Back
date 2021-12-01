74c3d848c28539f6abfb6936100bde9607dc8514
chibah-fawzi <fawzichibah@gmail.com>
2021-12-01 21:08:11 +0100

    set for build

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ todosRoute(app);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, '0.0.0.0', () => console.log(`Example app listening on port ${port}!`))
